641deb1e6eb1436b71f127d2b69403af4074764a
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-09-13 13:49:30 -0400

    Added function specification
    added blank function specification given by professor and also added my student details.

---
+++ Lab  1/lab1.js
@@ -0,0 +1,25 @@
+const questionOne = function questionOne(arr) {
+  // Implement question 1 here
+};
+
+const questionTwo = function questionTwo(arr) {
+  // Implement question 2 here
+};
+
+const questionThree = function questionThree(arr) {
+  // Implement question 3 here
+};
+
+const questionFour = function questionFour(num1, num2, num3) {
+  // Implement question 4 here
+};
+
+module.exports = {
+  firstName: "Ankit",
+  lastName: "Kachhadiya",
+  studentId: "20007140",
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+};
