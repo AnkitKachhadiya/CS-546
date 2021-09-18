42109ca7ba8770453b1422a4bdacfcb44bef5d8c
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-09-18 17:12:37 -0400

    solved sub question 1 for main question 1

--- Lab 2/arrayUtils.js
+++ Lab 2/arrayUtils.js
@@ -1,1 +1,67 @@
-console.log('Export');
+const average = (arr) => {
+    isArgumentArray(arr);
+    isArrayEmpty(arr);
+    doesArrayHasAllArrays(arr);
+    doesArrayOfArrayHasValues(arr);
+
+    let sum = 0;
+    let totalCount = 0;
+    arr.forEach((currentArray) => {
+        doesArrayHasAllNumbers(currentArray);
+
+        currentArray.forEach((currentElement) => {
+            sum = sum + currentElement;
+            totalCount++;
+        });
+    });
+
+    return Math.round(sum / totalCount);
+};
+
+const modeSquared = (arr) => {};
+
+const medianElement = (arr) => {};
+
+const merge = (arr) => {};
+
+//All validations
+const isArgumentArray = (arr) => {
+    if (!Array.isArray(arr)) {
+        throw "Error: Invalid argument passed. Expected array.";
+    }
+};
+
+const isArrayEmpty = (arr) => {
+    if (arr.length < 1) {
+        throw "Error: Empty array passed.";
+    }
+};
+
+const doesArrayHasAllArrays = (arr) => {
+    if (!arr.every((currentArray) => Array.isArray(currentArray))) {
+        throw "Error: All elements in an array must have array as an element.";
+    }
+};
+
+const doesArrayOfArrayHasValues = (arr) => {
+    if (!arr.every((currentArray) => currentArray.length > 0)) {
+        throw "Error: All elements in an array must have array as an element with minimum 1 element in that array.";
+    }
+};
+
+const doesArrayHasAllNumbers = (arr) => {
+    const isAllElementsNumbers = arr.every((currentElement) => {
+        return typeof currentElement === "number" && !isNaN(currentElement);
+    });
+
+    if (!isAllElementsNumbers) {
+        throw "Error: All elements in an array must have array as an element containing only numbers.";
+    }
+};
+
+module.exports = {
+    average,
+    modeSquared,
+    medianElement,
+    merge,
+};
--- Lab 2/index.js
+++ Lab 2/index.js
@@ -1,1 +1,15 @@
-console.log('Import');
+const arrayUtils = require("./arrayUtils");
+
+try {
+    console.log(arrayUtils.average([[0.1, 2, 0.3]]));
+    // Returns: 2
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    console.log(arrayUtils.average(["guitar", 1, 3, "apple"]));
+    // throws an error
+} catch (error) {
+    console.error(error);
+}
