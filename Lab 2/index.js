7f2f02b1fe5966ec48c0b925f2a198ed09f172e6
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-09-20 19:55:04 -0400

    Done with the setup for question 3 lab 2 lab work

--- Lab 2/index.js
+++ Lab 2/index.js
@@ -1,44 +1,45 @@
 const arrayUtils = require("./arrayUtils");
 const stringUtils = require("./stringUtils");
+const objUtils = require("./objUtils");
 
-try {
-    console.log(arrayUtils.average([[1, 2, 3]]));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(arrayUtils.average([[1, 2, 3]]));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(arrayUtils.modeSquared([1, 2, 3, 3, 4]));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(arrayUtils.modeSquared([1, 2, 3, 3, 4]));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(arrayUtils.medianElement([5, 6, 6, 7]));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(arrayUtils.medianElement([5, 6, 6, 7]));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(arrayUtils.merge([1, 2, 3], [3, 1, 2]));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(arrayUtils.merge([1, 2, 3], [3, 1, 2]));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(stringUtils.sortString("123 FOO BAR!"));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(stringUtils.sortString("123 FOO BAR!"));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(stringUtils.replaceChar("Daddy", 2));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(stringUtils.replaceChar("Daddy", 2));
+// } catch (error) {
+//     console.error(error);
+// }
 
-try {
-    console.log(stringUtils.mashUp("Patrick", "Hill", "$"));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(stringUtils.mashUp("Patrick", "Hill", "$"));
+// } catch (error) {
+//     console.error(error);
+// }
---
+++ Lab 2/objUtils.js
@@ -0,0 +1,11 @@
+const computeObjects = (arr, func) => {};
+
+const commonKeys = (obj1, obj2) => {};
+
+const flipObject = (obj) => {};
+
+module.exports = {
+    computeObjects,
+    commonKeys,
+    flipObject,
+};
