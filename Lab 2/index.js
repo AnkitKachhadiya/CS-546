aee1153d4394e177dc4d1ae5a4a981c0c54d541d
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-09-20 14:41:11 -0400

    Solved suv question 1 for question 2 lab 2 lab work

--- Lab 2/index.js
+++ Lab 2/index.js
@@ -1,32 +1,32 @@
 const arrayUtils = require("./arrayUtils");
 const stringUtils = require("./stringUtils");
 
-try {
-    console.log(arrayUtils.average([[1, 2, 3]]));
-} catch (error) {
-    console.error(error);
-}
+// try {
+//     console.log(arrayUtils.average([[1, 2, 3]]));
+// } catch (error) {
+//     console.error(error);
+// }
+
+// try {
+//     console.log(arrayUtils.modeSquared([1, 2, 3, 3, 4]));
+// } catch (error) {
+//     console.error(error);
+// }
+
+// try {
+//     console.log(arrayUtils.medianElement([5, 6, 6, 7]));
+// } catch (error) {
+//     console.error(error);
+// }
+
+// try {
+//     console.log(arrayUtils.merge([1, 2, 3], [3, 1, 2]));
+// } catch (error) {
+//     console.error(error);
+// }
 
 try {
-    console.log(arrayUtils.modeSquared([1, 2, 3, 3, 4]));
-} catch (error) {
-    console.error(error);
-}
-
-try {
-    console.log(arrayUtils.medianElement([5, 6, 6, 7]));
-} catch (error) {
-    console.error(error);
-}
-
-try {
-    console.log(arrayUtils.merge([1, 2, 3], [3, 1, 2]));
-} catch (error) {
-    console.error(error);
-}
-
-try {
-    console.log(stringUtils.sortString("ABWCEcec@$@1 124afdvABC"));
+    console.log(stringUtils.sortString("098765432110124"));
 } catch (error) {
     console.error(error);
 }
--- Lab 2/stringUtils.js
+++ Lab 2/stringUtils.js
@@ -1,8 +1,60 @@
-const sortString = (str) => {};
+const sortString = (str) => {
+    isArgumentString(str);
+    isStringEmpty(str);
+
+    const arrayedString = str.split("");
+
+    const allChars = [];
+    const allNumbers = [];
+    const allSpaces = [];
+    const allSpecialChars = [];
+
+    arrayedString.forEach((currentElement) => {
+        //check for number
+        if (!isNaN(currentElement) && !isNaN(parseFloat(currentElement))) {
+            allNumbers.push(currentElement);
+            return;
+        }
+
+        //check for characters
+        if (currentElement.toLowerCase() !== currentElement.toUpperCase()) {
+            allChars.push(currentElement);
+            return;
+        }
+
+        //check for empty space or empty string
+        if (currentElement === " ") {
+            allSpaces.push(currentElement);
+            return;
+        }
+
+        allSpecialChars.push(currentElement);
+    });
+
+    allChars.sort();
+    allNumbers.sort();
+
+    return [...allChars, ...allSpecialChars, ...allNumbers, ...allSpaces].join(
+        ""
+    );
+};
 
 const replaceChar = (str, index) => {};
 
 const mashUp = (str1, str2, char) => {};
+
+//All validations
+const isArgumentString = (str) => {
+    if (typeof str !== "string") {
+        throw "Error: Invalid argument passed. Expected string.";
+    }
+};
+
+const isStringEmpty = (str) => {
+    if (!str.trim() || str.length < 1) {
+        throw "Error: Empty string passed.";
+    }
+};
 
 module.exports = {
     sortString,
