e02d26fcf2c24f9f8152890873e1cc125d08030e
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-10-08 12:12:28 -0400

    Initial setup for lab 5

---
+++ Lab 5/app.js
@@ -0,0 +1,13 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Hello world");
+});
+
+app.listen(PORT, () => {
+    console.log("running...");
+});
